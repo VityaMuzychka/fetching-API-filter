94f03f9849ed1a7bbdc56bef8f76157b811fa3ea
VityaMuzychka <mirrorvitya@gmail.com>
2023-11-25 18:32:42 +0200

    the project is added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import Axios from 'axios';
+
 
 function App() {
+  const [listOfUsers, setListOfUsers] = useState([]);
+  const [searchWord, setSearchWord] = useState('');
+  
+  useEffect(() => {
+   Axios.get("https://jsonplaceholder.typicode.com/users/").then((res) => {
+   setListOfUsers(res.data);
+   })
+  }, [])
+  
+  const filteredUsers = listOfUsers.filter(user => {
+    return user.name.toLowerCase().includes(searchWord.toLowerCase());
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className='userHeader'>
+      <input type='text' placeholder='Search...' onChange={(event) => setSearchWord(event.target.value)}/>
+     </div>
+     <div className='userDisplay'>
+      {filteredUsers.map(user => {
+        return (
+          <div className='userInfo'>
+            <h1>Name: {user.name}</h1>
+            <h2>Username: {user.username}</h2>
+            <h2>Email: {user.email}</h2>
+            <h2>Address: {user.address.city}</h2>
+          </div>
+        )
+      })}
+     </div>
     </div>
   );
 }
